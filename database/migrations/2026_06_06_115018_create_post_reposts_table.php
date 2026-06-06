5ea4e015b7f1c4884953052a0ba4bb8fc9627a30
vanhuy2005 <nguyen.van.quang.huy.2105@gmail.com>
2026-06-06 19:32:19 +0700

    hot-fix: build fail due to cascade

--- database/migrations/2026_06_06_115018_create_post_reposts_table.php
+++ database/migrations/2026_06_06_115018_create_post_reposts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('post_reposts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('no action');
             $table->timestamps();
 
             $table->unique(['user_id', 'post_id']);
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->index(['post_id', 'created_at']);
         });
     }
-
+    
     /**
      * Reverse the migrations.
      */
