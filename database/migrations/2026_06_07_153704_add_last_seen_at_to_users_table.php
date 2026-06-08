04fcafb24b932a3632883a83a99d3b5882c30a59
Nguyen Van Quang Huy <159224937+vanhuy2005@users.noreply.github.com>
2026-06-08 23:16:58 +0700

    fix-migration (#47)
    Co-authored-by: HoshinoAquamarine01 <phamgiabao141105@gmail.com>

--- database/migrations/2026_06_07_153704_add_last_seen_at_to_users_table.php
+++ database/migrations/2026_06_07_153704_add_last_seen_at_to_users_table.php
@@ -11,9 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('last_seen_at')->nullable();
-        });
+        if (! Schema::hasColumn('users', 'last_seen_at')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->timestamp('last_seen_at')->nullable();
+            });
+        }
     }
 
     /**
@@ -21,8 +23,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('last_seen_at');
-        });
+        if (Schema::hasColumn('users', 'last_seen_at')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('last_seen_at');
+            });
+        }
     }
 };
