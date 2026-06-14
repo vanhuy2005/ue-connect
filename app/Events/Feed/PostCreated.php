c7016a98d55a1881dbae1448189b1c157c0baf2a
Nguyen Van Quang Huy <159224937+vanhuy2005@users.noreply.github.com>
2026-06-14 16:59:18 +0700

    refactor: change broadcast events to implement ShouldBroadcast instead of ShouldBroadcastNow for async queue broadcasting (#85)

--- app/Events/Feed/PostCreated.php
+++ app/Events/Feed/PostCreated.php
@@ -6,11 +6,11 @@ use App\Models\Post;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PostCreated implements ShouldBroadcastNow
+class PostCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
