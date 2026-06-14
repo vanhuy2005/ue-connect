619d862cbd000454c590cc91fc4aa23b6821cd1e
vanhuy2005 <nguyen.van.quang.huy.2105@gmail.com>
2026-06-14 16:58:11 +0700

    refactor: change broadcast events to implement ShouldBroadcast instead of ShouldBroadcastNow for async queue broadcasting

--- app/Events/Messaging/ConversationUpdated.php
+++ app/Events/Messaging/ConversationUpdated.php
@@ -7,11 +7,11 @@ use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ConversationUpdated implements ShouldBroadcastNow
+class ConversationUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
--- app/Events/Messaging/MessageSent.php
+++ app/Events/Messaging/MessageSent.php
@@ -6,11 +6,11 @@ use App\Models\Message;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent implements ShouldBroadcastNow
+class MessageSent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
